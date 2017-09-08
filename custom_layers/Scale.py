6593768fc9601c24e06efd29c50c14dce946d482
Eric Vandder Putten <ejvander9@gmail.com>
2017-09-07 21:13:38 -0500

    Updated Scale layer

--- custom_layers/Scale.py
+++ custom_layers/Scale.py
@@ -1,21 +1,43 @@
-from keras.layers import Layer
+from keras.engine import Layer, InputSpec
 from keras import backend as K
 
+
+
 class Scale(Layer):
-  def __init__(self, nb_filter, bias, **kwargs):
-    super(Scale, self).__init__(**kwargs)
-    self.bias = bias
-    self.nb_filter = nb_filter
+    uid_ = 1
+    '''Learns a set of weights and biases used for scaling the input data.
+    '''
+    def __init__(self, weights=None, axis=-1, momentum = 0.9, beta_init='zero', gamma_init='one', **kwargs):
+        self.momentum = momentum
+        self.axis = axis
+        self.beta_init = beta_init
+        self.gamma_init = gamma_init
+        self.initial_weights = weights
+        super(Scale, self).__init__(**kwargs)
+        self.name = "Scale_" + str(Scale.uid_)
+        Scale.uid_ += 1
 
-  def build(self, input_shape):
-    self.scale = self.add_weight(name="scale", shape=(self.nb_filter,), initializer='uniform', trainable=True)
-    if(self.bias):
-      self.b = self.add_weight(name="bias", shape=(self.nb_filter,), initializer='zero', trainable=True)
-    else:
-      self.b = 0;
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        shape = (int(input_shape[self.axis]),)
 
-  def call(self, x, mask=None):
-    out = x*K.reshape(self.scale, (1, 1, 1, self.nb_filter))
-    if(self.bias):
-      out += K.reshape(self.b, (1, 1, 1, self.nb_filter))
-    return out
+        self.gamma = self.add_weight(shape=shape, initializer=self.gamma_init, trainable=True, name="{}_gamma".format(self.name))
+        self.beta = self.add_weight(shape=shape, initializer=self.beta_init, trainable=True, name="{}_beta".format(self.name))
+        self.trainable_weights = [self.gamma, self.beta]
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+
+    def call(self, x, mask=None):
+        input_shape = self.input_spec[0].shape
+        broadcast_shape = [1] * len(input_shape)
+        broadcast_shape[self.axis] = input_shape[self.axis]
+
+        out = K.reshape(self.gamma, broadcast_shape) * x + K.reshape(self.beta, broadcast_shape)
+        return out
+
+    def get_config(self):
+        config = {"momentum": self.momentum, "axis": self.axis}
+        base_config = super(Scale, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
